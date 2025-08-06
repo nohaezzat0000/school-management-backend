3d128a0c3c44eb7778e05cb0e5054e6da1348e24
Ahmed-Fa1sal <DerRiese765@gmail.com>
2025-08-06 12:34:08 +0300

    adding redis config

--- src/main/java/com/school/SchoolManagementApplication.java
+++ src/main/java/com/school/SchoolManagementApplication.java
@@ -2,9 +2,11 @@ package com.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableCaching
 public class SchoolManagementApplication {
 
 	public static void main(String[] args) {
